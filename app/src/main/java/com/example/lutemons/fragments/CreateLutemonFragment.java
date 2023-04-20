36109c2e0198a54b51b9779087b780e0cb68b4d7
Lasstuu <lassituo@gmail.com>
2023-04-20 11:31:38 +0300

    Create lutemonin työstö

--- app/src/main/java/com/example/lutemons/fragments/CreateLutemonFragment.java
+++ app/src/main/java/com/example/lutemons/fragments/CreateLutemonFragment.java
@@ -7,20 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
+import com.example.lutemons.Lutemon;
 import com.example.lutemons.R;
+import com.example.lutemons.Storage;
 
 
 public class CreateLutemonFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public CreateLutemonFragment() {
         // Required empty public constructor
@@ -39,5 +36,74 @@ public class CreateLutemonFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_lutemon, container, false);
         return view;
+    }
+    public void createLutemon(View view){
+        EditText etLutemonName = view.findViewById(R.id.etLutemonName);
+        RadioGroup rgLutemonClass = view.findViewById(R.id.rgLutemonClass);
+        RadioGroup rgLutemonColor = view.findViewById(R.id.rgLutemonColor);
+
+        switch (rgLutemonColor.getCheckedRadioButtonId()) {
+
+            case R.id.rbWhite:
+                switch (rgLutemonClass.getCheckedRadioButtonId()) {
+                    case R.id.rbTank:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "White", "Tank", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbWarrior:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "White", "Warrior", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbMonk:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "White", "Monk", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                }
+            case R.id.rbGreen:
+                switch (rgLutemonClass.getCheckedRadioButtonId()) {
+                    case R.id.rbTank:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Green", "Tank", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbWarrior:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Green", "Warrior", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbMonk:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Green", "Monk", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                }
+            case R.id.rbPink:
+                switch (rgLutemonClass.getCheckedRadioButtonId()) {
+                    case R.id.rbTank:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Pink", "Tank", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbWarrior:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Pink", "Warrior", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbMonk:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Pink", "Monk", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                }
+            case R.id.rbOrange:
+                switch (rgLutemonClass.getCheckedRadioButtonId()) {
+                    case R.id.rbTank:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Orange", "Tank", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbWarrior:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Orange", "Warrior", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbMonk:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Orange", "Monk", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                }
+            case R.id.rbBlack:
+                switch (rgLutemonClass.getCheckedRadioButtonId()) {
+                    case R.id.rbTank:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Black", "Tank", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbWarrior:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Black", "Warrior", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                    case R.id.rbMonk:
+                        Storage.getInstance().addLutemon(new Lutemon(etLutemonName.getText().toString(), "Black", "Monk", 1, 1, 1, 1, 1, 0, 0));
+                        break;
+                }
+        }
     }
 }
