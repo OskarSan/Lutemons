f59159f10750d299f65075da259f7d481c3f57a2
Lasstuu <lassituo@gmail.com>
2023-04-20 10:56:44 +0300

    Create lutemonin työstö

--- app/src/main/java/com/example/lutemons/fragments/CreateLutemonFragment.java
+++ app/src/main/java/com/example/lutemons/fragments/CreateLutemonFragment.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import com.example.lutemons.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CreateLutemonFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CreateLutemonFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -30,37 +26,18 @@ public class CreateLutemonFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentA.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreateLutemonFragment newInstance(String param1, String param2) {
-        CreateLutemonFragment fragment = new CreateLutemonFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_lutemon, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_lutemon, container, false);
+        return view;
     }
 }
